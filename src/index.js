52932b1e083566e91caa5714e559017a93f65fe2
bnj ducal <ducaubenjamin@gmail.com>
2022-04-28 16:49:25 +0200

    mise à jour du renderDom pour la version 18 avec root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// change for React Version 18
+
+//ReactDOM from 'react-dom' to :
+import ReactDOM from 'react-dom/client';
+
+// Replace render by the new root render
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+// import {createRoot} from 'react-dom/client'
